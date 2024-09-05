6fc3aee312dbbb7703313ca912ee655f3d940cf2
MariaPérezPulles <mperez@t2client.com>
2024-09-05 04:16:24 +0200

    fixed dice history

--- components/dice-component/dice-component.js
+++ components/dice-component/dice-component.js
@@ -1,6 +1,7 @@
+// dice-component.js
+
 let lastSides = null; // Para almacenar el último tipo de dado
 let lastTiradas = null; // Para almacenar el último número de tiradas
-let history = []; // Almacenar el historial de tiradas
 
 // Función para lanzar un dado
 function rollDice(sides) {
@@ -17,25 +18,16 @@ function rollDice(sides) {
         const result = Math.floor(Math.random() * sides) + 1;
         results.push(result);
 
-        // Crear un nuevo dado (cuadrado) para cada tirada
         const dice = document.createElement('div');
         dice.classList.add('dice');
         dice.textContent = result; // Mostrar el resultado dentro del dado
-
-        // Añadir el dado al contenedor principal
         diceContainer.appendChild(dice);
     }
 
-    // Ordenar los resultados en orden descendente
     const sortedResults = results.sort((a, b) => b - a);
-
-    // Tomar los tres resultados más altos
     const topThreeResults = sortedResults.slice(0, 3);
-
-    // Calcular el total sumando los tres resultados más altos
     const total = topThreeResults.reduce((acc, curr) => acc + curr, 0);
 
-    // Mostrar el total
     const totalP = document.createElement('p');
     totalP.className = "fs-4";
     totalP.innerHTML = `Total de los 3 más altos: <span id="total">${total}</span>`;
@@ -48,8 +40,12 @@ function rollDice(sides) {
     // Habilitar el botón de "Repetir tirada"
     document.getElementById('repeat-button').disabled = false;
 
-    // Añadir la tirada al historial
-    addRollToHistory(results, total);
+    // Llamar a la función para agregar la tirada al historial
+    if (typeof addRollToHistory === 'function') {
+        addRollToHistory(results, total);
+    } else {
+        console.error('addRollToHistory no está definida');
+    }
 }
 
 // Función para repetir la última tirada
